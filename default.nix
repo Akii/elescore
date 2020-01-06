7658f52804696474f248ab96890df6596163da45
Akii <zedd@akii.de>
2020-01-06 11:02:46 +0100

    Fix build by pinning nixpkgs to older version

--- default.nix
+++ default.nix
@@ -8,6 +8,13 @@ let
       };
     };
   };
+  
+  nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "cc2b6f32a9073748965a4f2e7c874a0acc4c3838";
+    sha256 = "05zblr02lkfq83zrrnx88c3y5ras6k5a4dmipv651brd67r4v6qg";
+  };
 
   pkgs = import <nixpkgs> { inherit config; };
 
